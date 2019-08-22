fa422d29d940f8fe56fa4387d9a3319cd3331208
Federica Macchi <federica.macchi3@gmail.com>
2019-08-22 21:47:07 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,9 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return { friends: [...state.friends, { name: 'Joe', hometown: 'Boston', id: 101}]}
+        case 'REMOVE_FRIEND':
+            return { friends: state.friends.filter(friend => friend.id !== 101)}
+    }
+    return state
 }
